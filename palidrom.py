205a6f0dedaaa2b34ee3ebde8bf157dab3f6e8cf
Christian Wichmann <cw1981@gmx.de>
2013-09-01 15:08:56 +0200

    Added palindrom script.

---
+++ palidrom.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Erkennung von Palindromen
+
+@author: Christian Wichmann
+"""
+
+def stripDown(string):
+    return "".join(filter(str.isalnum, string.lower()))
+
+if __name__ == "__main__":
+    
+    print("Palindromerkennung...")
+    text = str(input("Bitte Text eingeben: "))
+    
+    if stripDown(text) == stripDown(text[::-1]):
+        print("Palindrom")
+    else:
+        print("Kein Palindrom")
+
